96ca9600232d531c398f4653c34dd8253b85ab2a
Ahmed Sayed Abdelsalam <e.eng.ahmedsayed@gmail.com>
2025-07-26 03:47:05 +0300

    :lipstick: Add fronted form

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express')
+import express from 'express'
 const app = express()
 
 const PORT = 3000
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static('public'))
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
